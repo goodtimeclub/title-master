be476e7e7aa0ebe9676e939fe00f7f971db236f3
goodtimeclub <dcopeland@NMD-DCopeland.local>
2015-07-22 15:12:12 -0400

    increment version to 1.6.0

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TITLEMASTER_VERSION') or define('TITLEMASTER_VERSION', '1.5.9');
+defined('TITLEMASTER_VERSION') or define('TITLEMASTER_VERSION', '1.6.0');
 
 $config['name']='Title Master';
 $config['version']= TITLEMASTER_VERSION;
